1871f80af6a6e02833377e470c685d5b6d24843c
pvallerie <petevallerie@gmail.com>
2021-03-24 19:11:41 -0400

    Minor form style and state logic

--- src/components/business/Login.js
+++ src/components/business/Login.js
@@ -1,19 +1,59 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
+
+import "../../index.css"
 
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
 
 function Login() {
+  const [credentials, setCredentials] = useState(null)
+
+  const handleChange = event => {
+    event.persist()
+
+    setCredentials({ ...credentials, [event.target.name]: event.target.value })
+
+    console.log(credentials)
+
+    // setCredentials(prevState => {
+    //   const updatedField = { [event.target.name]: event.target.value }
+    //   const editCredentials = Object.assign({}, prevState, updatedField)
+    //   return editCredentials
+    // })
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    console.log(credentials)
+  }
+
   return(
-    <Form>
+    <Form
+      className="login-form"
+      onSubmit={handleSubmit}
+      >
       <Form.Group controlId="formBasicEmail">
-        <Form.Control type="email" placeholder="Email" />
+        <h2>Welcome Back!</h2>
+        <Form.Control
+          onChange={handleChange}
+          name="email"
+          type="email"
+          placeholder="Email"
+        />
       </Form.Group>
       <Form.Group controlId="formBasicPassword">
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          onChange={handleChange}
+          name="password"
+          type="password"
+          placeholder="Password"
+        />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button
+        variant="primary"
+        type="submit"
+      >
         Login
       </Button>
 
